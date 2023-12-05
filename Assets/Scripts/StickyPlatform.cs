e19ef70b494d84e8954c47caa8dc775eb1a043df
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2023-12-05 18:23:17 +0700

    Add Moving Platforms

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "Player")
+    //    {
+    //        collision.gameObject.transform.SetParent(transform);
+    //    }
+    //}
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "Player")
+    //    {
+    //        collision.gameObject.transform.SetParent(null);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
