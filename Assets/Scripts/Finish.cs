a48c7d3733164df5ea07786581d9be527f3fba79
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2023-12-10 16:39:19 +0700

    Start & Game Over Menu

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -16,7 +16,7 @@ public class Finish : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
             levelCompleted = true;
