3904d6491b4ad45b8c05338e9e9709907f136a38
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2024-01-09 17:22:23 +0700

    Add player's head stomping feature, causing boss to die on impact

--- Assets/Devil/Devil.cs
+++ Assets/Devil/Devil.cs
@@ -1,78 +1,4 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class Devil : MonoBehaviour
-//{
-//    SpriteRenderer sprite;
-
-
-//    [SerializeField] public GameObject[] movePoints; // Mảng chứa các điểm di chuyển của Devil
-//    public Spear spearPrefab; // Prefab của Spear
-//    public float speed = 1;
-
-//    private int currentMovePointIndex = 0;
-//    private float shotTime = 0;
-
-//    void Start()
-//    {
-//        shotTime = Time.time;
-//        sprite = GetComponent<SpriteRenderer>();
-//        sprite.flipX = true;
-//    }
-
-//    void Update()
-//    {
-//        MoveDevil();
-//        ShootSpear();
-//    }
-
-//    void MoveDevil()
-//    {
-//        if (movePoints.Length == 0)
-//        {
-//            Debug.LogError("No move points assigned to Devil.");
-//            return;
-//        }
-
-//        // Di chuyển đến điểm di chuyển và đảo hướng khi đến gần điểm cuối
-//        transform.position = Vector3.MoveTowards(transform.position, movePoints[currentMovePointIndex].transform.position, speed * Time.deltaTime);
-//        if (Vector3.Distance(transform.position, movePoints[currentMovePointIndex].transform.position) < 0.1f)
-//        {
-//            currentMovePointIndex++;
-//            if (currentMovePointIndex >= movePoints.Length)
-//            {
-//                currentMovePointIndex = 0;
-//            }
-//            sprite.flipX = !sprite.flipX;
-//        }
-//    }
-
-//    void ShootSpear()
-//    {
-//        if (Time.time - shotTime > speed)
-//        {
-//            shotTime = Time.time;
-
-//            // Xác định hướng của giáo dựa trên hướng di chuyển của Devil
-//            Vector3 direction = movePoints[currentMovePointIndex].transform.position - transform.position;
-//            float facingDirection = Mathf.Sign(direction.x); // Lấy dấu của phần x của hướng
-
-//            Spear newSpear = Instantiate(spearPrefab, transform.position, Quaternion.identity);
-
-//            // Truyền hướng cho giáo
-//            newSpear.SetDirection(facingDirection);
-
-//            // Đặt tốc độ cho giáo
-//            newSpear.speed = speed;
-
-//            // lật hình ảnh của giáo
-//            newSpear.transform.localScale = new Vector3(movePoints[currentMovePointIndex].transform.position.x > transform.position.x ? 1 : -1, 1, 1);
-
-//        }
-//    }
-//}
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -86,6 +12,9 @@ public class Devil : MonoBehaviour
 
     private int currentMovePointIndex = 0;
     private float shotTime = 0;
+
+    // ref -> player để jump khi đạp vào devil
+    [SerializeField]private PlayerMovement playerMovement;
 
     void Start()
     {
@@ -136,6 +65,14 @@ public class Devil : MonoBehaviour
 
             // Lật hình ảnh của giáo
             newSpear.transform.localScale = new Vector3(facingDirection, 1, 1);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            playerMovement.DoubleJump();
         }
     }
 }
